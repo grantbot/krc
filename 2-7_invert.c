91895e6f0f3a23bccff829e11cd91b85f45a4a64
Grant Wu <grant.wu@cloverhealth.com>
2016-09-26 12:57:37 -0700

    ch2: cleaner invertbits

--- 2-7_invert.c
+++ 2-7_invert.c
@@ -8,13 +8,4 @@ bit in a byte is p = 7.
 
 #include <stdio.h>
 
-int invertbits(int x, int p, int n) {
-  int mask;
-  int inverted;
-
-  mask = ~(~0 << n) << (p + 1 - n); // Make n-bit mask and move into position
-  inverted = ~x & mask;             // Get inverted bits to set on result
-  x = x & ~mask; // Zero out desired bits so we can set them anew
-
-  return x | inverted; // Set new inverted bits
-}
+int invertbits(int x, int p, int n) { return x ^ (~(~0 << n)) << (p + 1 - n); }
